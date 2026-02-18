20a2bae1a34ae91d20efaaef96f727678cafbda1
Seth Battis <seth@battis.net>
2026-02-17 20:39:24 -0500

    fix: use @oauth2-cli/qui-cli's `Unregistered` namespace

--- packages/oauth2-cli/sky-api/src/SkyAPIPlugin.ts
+++ packages/oauth2-cli/sky-api/src/SkyAPIPlugin.ts
@@ -1,72 +1,93 @@
-import {OAuth2Plugin, Configuration, Token, Client} from '@oauth2-cli/qui-cli/dist/Extend.js';
-import * as OAuth2 from '@oauth2-cli/qui-cli/dist/Export.js'
+import * as OAuth2CLI from '@oauth2-cli/qui-cli/dist/Unregistered.js';
 import { Colors } from '@qui-cli/colors';
 import { Env } from '@qui-cli/env';
 import * as Plugin from '@qui-cli/plugin';
-import * as requestish from 'requestish'
+import * as requestish from 'requestish';
 
-type Credentials = OAuth2.Credentials & {subscription_key: string}
+type Credentials = OAuth2CLI.Credentials & { subscription_key: string };
 
-export type SkyConfiguration = Configuration & {credentials?: Partial<Credentials>}
+export type Configuration = OAuth2CLI.Configuration & {
+  credentials?: Partial<Credentials>;
+};
 
-export class SkyAPIPlugin extends OAuth2Plugin {
-  private subscription_key?: string = undefined
+export class SkyAPIPlugin extends OAuth2CLI.OAuth2Plugin<
+  Credentials,
+  OAuth2CLI.Client<Credentials>
+> {
+  private subscription_key?: string = undefined;
 
   public constructor() {
     super('@oauth2-cli/sky-api');
     super.configure({
-      credentials:{
+      credentials: {
         issuer: 'https://oauth2.sky.blackbaud.com'
-},
-      base_url:'https://api.sky.blackbaud.com',
-      man: { heading: 'Sky API options',
-        text:[
-          `The OAuth 2.0 ${Colors.keyword('refresh_token')} is read from the `+
-          `${Colors.varName('SKY_REFRESH_TOKEN')} environment variable, if `+
-          `present.`,
-          `The Sky API subscription access key is read from the `+
-          `${Colors.varName('SKY_SUBSCRIPTION_KEY')} environment variable, if `+
-          `present. See `+
-          `${Colors.url('https://developer.blackbaud.com/subscriptions/')} `+
-          `for more information.`]
-       },
+      },
+      base_url: 'https://api.sky.blackbaud.com',
+      man: {
+        heading: 'Sky API options',
+        text: [
+          `The Sky API ${Colors.keyword('subscription_key')} is read from the ` +
+            `${Colors.varName('SKY_SUBSCRIPTION_KEY')} environment variable, if ` +
+            `present. See ` +
+            `${Colors.url('https://developer.blackbaud.com/subscriptions/')} ` +
+            `for more information.`,
+          `The OAuth 2.0 ${Colors.keyword('refresh_token')} is read from the ` +
+            `${Colors.varName('SKY_REFRESH_TOKEN')} environment variable, if ` +
+            `present.`
+        ]
+      },
+      url: {
+        client_id: 'https://developer.blackbaud.com/apps/'
+      },
       env: {
         client_id: 'SKY_CLIENT_ID',
         client_secret: 'SKY_CLIENT_SECRET',
         scope: 'SKY_SCOPE',
-        redirect_uri: 'SKY_REDIRECT_URI',
+        redirect_uri: 'SKY_REDIRECT_URI'
       },
       suppress: {
         issuer: true,
         authorization_endpoint: true,
         token_endpoint: true,
+        base_url: true
       },
-      storage:new Token.EnvironmentStorage('SKY_REFRESH_TOKEN')
+      storage: new OAuth2CLI.Token.EnvironmentStorage('SKY_REFRESH_TOKEN')
     });
   }
 
-  public configure({credentials, ...options}: SkyConfiguration = {}): void {
-    const {subscription_key, ...rest} = credentials || {};
-    super.configure({credentials:rest, ...options});
-    this.subscription_key = Plugin.hydrate(subscription_key, this.subscription_key);
+  public configure({ credentials, ...options }: Configuration = {}): void {
+    const { subscription_key, ...rest } = credentials || {};
+    super.configure({ credentials: rest, ...options });
+    this.subscription_key = Plugin.hydrate(
+      subscription_key,
+      this.subscription_key
+    );
   }
 
   public async init(args: Plugin.ExpectedArguments<typeof this.options>) {
     await super.init(args);
-    this.configure({credentials: {subscription_key: await Env.get({key: 'SKY_SUBSCRIPTION_KEY'})}})
+    this.configure({
+      credentials: {
+        subscription_key: await Env.get({ key: 'SKY_SUBSCRIPTION_KEY' })
+      }
+    });
   }
 
-  protected instantiateClient(options: OAuth2.ClientOptions): Client {
+  protected instantiateClient(
+    options: OAuth2CLI.ClientOptions<Credentials>
+  ): OAuth2CLI.Client<Credentials> {
     if (!this.subscription_key) {
       throw new Error('No subscription access key is defined.');
     }
-        let {headers} = options.inject || {};
+    let { headers } = options.inject || {};
     headers = requestish.Headers.merge(headers, {
-          'Bb-Api-Subscription-Key': this.subscription_key})
-    return new Client({
+      'Bb-Api-Subscription-Key': this.subscription_key
+    });
+    return new OAuth2CLI.Client({
       ...options,
-      inject:{
-        headers}
+      inject: {
+        headers
+      }
     });
   }
 }
